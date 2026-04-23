a074456131ded2a99b96571d779589fab52f99b1
Tiago Heitzmann <tiagoheitzmann@gmail.com>
2026-04-23 18:19:13 -0300

    Bug correction 2

--- backend/api.py
+++ backend/api.py
@@ -1,31 +1,8 @@
 import os
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-from datetime import datetime
-print("🚀 API iniciando...")
-from storage import listar_audios, url_audio
 
 app = FastAPI()
 
-def nome_audio_hoje():
-    hoje = datetime.now().strftime('%Y-%m-%d')
-    return f'{hoje}.mp3'
-
 @app.get("/status")
 def status():
-    audios = listar_audios()
-    if nome_audio_hoje() in audios:
-        return {"pronto": True}
-    return {"pronto": False, "mensagem": "Notícias ainda chegando... 11:00 libera!"}
-
-@app.get("/audio")
-def audio_do_dia():
-    audios = listar_audios()
-    nome = nome_audio_hoje()
-    if nome in audios:
-        return RedirectResponse(url=url_audio(nome))
-    return {"erro": "Áudio de hoje ainda não está pronto"}
-
-@app.get("/lista")
-def listar():
-    return {"audios": listar_audios()}
+    return {"pronto": True, "teste": "API funcionando!"}
